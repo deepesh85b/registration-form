4409a08fbdd60f9a1b3c26aa5ce244a7e72a9229
deepesh <deepeshbhardwaj85@gmail.com>
2023-10-09 02:14:43 +0530

    ss

--- index.js
+++ index.js
@@ -5,27 +5,19 @@ const dotenv = require("dotenv");
 
 const app = express();
 dotenv.config();
+const username = process.env.MONGO_USERNAME;
 const password = process.env.MONGO_PASSWORD;
 
 const port = process.env.PORT || 3000;
 
-// Connect to MongoDB
-const connection = async () => {
-  try {
-    await mongoose.connect(
-      `mongodb+srv://deepesh85b:${password}@cluster0.fajufqy.mongodb.net/registrationFormDB`,
-      {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-      }
-    );
-    console.log("database connected!");
-  } catch (error) {
-    console.log("Error while connection with database", error.message);
+mongoose.connect(
+  `mongodb+srv://${username}:${password}@cluster0.fajufqy.mongodb.net/registrationFormDB`,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
   }
-}
+);
 
-connection();
 // Create a registration schema
 const registrationSchema = new mongoose.Schema({
   name: String,
@@ -46,25 +38,32 @@ app.get("/", (req, res) => {
 
 // Handle form submission and save data to MongoDB
 app.post("/register", async (req, res) => {
-  console.log(req.body);
-  const { name, email, password } = req.body;
-  // Create a new registration document
-  const registrationData = new Registration({
-    name,
-    email,
-    password,
-  });
-
   try {
-    // Save the document to MongoDB using await
-    await registrationData.save();
-    // find this registrationData in mongodb
-    const data = await Registration.findOne({
-      username: registrationData.username,
-    });
-    console.log(data);
-    res.redirect("/success");
+    // Extract data from the request body
+    const { name, email, password } = req.body;
+
+    // Check if a user with the same email exists in MongoDB
+    const existingUser = await Registration.findOne({ email });
+
+    if (!existingUser) {
+      // Create a new registration document
+      const registrationData = new Registration({
+        name,
+        email,
+        password,
+      });
+
+      // Save the document to MongoDB
+      await registrationData.save();
+
+      // Redirect to success page upon successful registration
+      res.redirect("/success");
+    } else {
+      // Redirect to an error page if the user already exists
+      res.redirect("/error");
+    }
   } catch (err) {
+    // Handle any errors that occur during registration
     console.error(err);
     res.redirect("/error");
   }
